a961d4a4a79571000f83eaa2a868c1e9e35a9886
javafuns <javafuns@about.me>
2016-12-13 17:43:01 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM node:4
 EXPOSE 8080
-COPY costmanager.js
-COPY index.html .
+COPY costmanager.js .
+COPY public/ ./
 RUN npm install --save google-cloud
 RUN npm install --save express
-RUN npm install --save socket.io
+#RUN npm install --save socket.io
 CMD node costmanager.js
--- costmanager.js
+++ costmanager.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-//app.use(express.static('public'));
-app.use('/', express.static(path.join(__dirname, 'index.html')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/add', function (req, res, next) {
     console.log("add");
 });
 
+app.get('/',function(req, res){
+    res.sendFile('public/index.html');
+});
+
+/**
 app.get('/get', function(req, res) {
     console.log("get");
 });
+*/
 
 if (module === require.main) {
   const server = app.listen(8080, () => {
-    const port = server.address().port;
-    console.log("App listening on port ${port}");
+    console.log("App listening on *:8080");
   });
 }
 module.exports = app;
