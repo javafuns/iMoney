01f96779d1942abda7d46c3f7a2c0fa180b739be
javafuns <javafuns@about.me>
2016-12-13 11:37:56 +0800

    update index.js

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type': 'text/html'});
-    res.end(fs.readFileSync(__dirname + '/index.html'));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-
-server.listen(8080, '127.0.0.1', () => {
-    console.log('Listening at: http://localhost:8080');
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
-io.listen(server).on('connection', function (socket) {
-    socket.on('message', function (msg) {
-        console.log('A request received: ', msg); 
-        socket.broadcast.emit('message', msg);
-    });
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
