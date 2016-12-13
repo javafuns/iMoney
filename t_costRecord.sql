0f5334c033772b8b1873559ba3daa6586b6e4f06
DuoyueBa <chenct@263.net>
2016-12-13 18:11:18 +0800

    Add files via upload

---
+++ t_costRecord.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 07, 2014 at 11:05 AM
+-- Server version: 5.5.34
+-- PHP Version: 5.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `imoney`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_costRecord`
+--
+
+CREATE TABLE IF NOT EXISTS `t_costRecord` (
+  `record_id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `cost` varchar(100) NOT NULL,
+  PRIMARY KEY (`record_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `t_costRecord`
+--
+
+INSERT INTO `t_costRecord` (`record_id`, `date`, `type`, `cost`) VALUES
+(1, '2016-12-01', 'food', '150'),
+(2, '2016-12-02', 'traffic', '30');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
